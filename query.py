24a6627cdaba5a8f55fc1d00268bbf113c934d49
Shaun Garwood <shaun.garwood@gmail.com>
2020-02-10 06:22:59 -0700

    better query

--- query.py
+++ query.py
@@ -10,15 +10,38 @@ mycol = mydb["2019-voters"]
 
 pp = pprint.PrettyPrinter(indent=4)
 
+boring_fields = [
+    "ADDRESS_LIBRARY_ID",
+    "CONGRESSIONAL",
+    "HOUSE_NUM",
+    "ID_REQUIRED",
+    "PERMANENT_MAIL_IN_VOTER",
+    "PRECINCT",
+    "PRECINCT_NAME",
+    "SPLIT",
+    "SPL_ID",
+    "STATE_HOUSE",
+    "STATE_SENATE",
+    "STATUS_CODE",
+    "STREET_NAME",
+    "STREET_TYPE",
+    "VOTER_STATUS_ID",
+    "_id"
+]
+
 def search(query):
     res = []
     for x in mycol.find(query): 
         res.append(x.copy())
     return res
 
+def search_name(name):
+    first, last = name.split(" ")
+    return search({"FIRST_NAME": first, "LAST_NAME": last})
+
 def clean_empty(entry):
     for key in list(entry):
-        if entry[key] == '':
+        if key in boring_fields or entry[key] == '':
             del entry[key]
 
 def print_res(res):
