28d4e917a722cb06ca2dfd0a44de3a46dc005c19
Shaun Garwood <shaun.garwood@gmail.com>
2020-02-10 06:44:41 -0700

    better search

--- query.py
+++ query.py
@@ -13,9 +13,11 @@ pp = pprint.PrettyPrinter(indent=4)
 boring_fields = [
     "ADDRESS_LIBRARY_ID",
     "CONGRESSIONAL",
+    "COUNTY_CODE",
     "HOUSE_NUM",
     "ID_REQUIRED",
     "PERMANENT_MAIL_IN_VOTER",
+    "PRE_DIR",
     "PRECINCT",
     "PRECINCT_NAME",
     "SPLIT",
@@ -33,13 +35,14 @@ def search(query):
     res = []
     for x in mycol.find(query): 
         res.append(x.copy())
+    [clean_res[x] for x in res]
     return res
 
 def search_name(name):
     first, last = name.split(" ")
     return search({"FIRST_NAME": first, "LAST_NAME": last})
 
-def clean_empty(entry):
+def clean_res(entry):
     for key in list(entry):
         if key in boring_fields or entry[key] == '':
             del entry[key]
@@ -47,7 +50,7 @@ def clean_empty(entry):
 def print_res(res):
     for x in res:
         voter_copy = x.copy()
-        clean_empty(voter_copy)
+        clean_res(voter_copy)
         pp.pprint(voter_copy)
 
 embed()
