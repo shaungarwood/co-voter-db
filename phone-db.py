3f40586464de432eec63dc2fab79b0f5e7e95d44
Shaun Garwood <shaun.garwood@gmail.com>
2020-02-11 06:38:40 -0700

    create phone collection

---
+++ phone-db.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import pymongo
+
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+mydb = myclient["voters"]
+mycol = mydb["2019-voters"]
+
+phone_col = mydb["phone-nums"]
+
+interesting_keys = [
+    'VOTER_ID',
+    'LAST_NAME',
+    'FIRST_NAME'
+]
+
+voters = mycol.find({"PHONE_NUM": {"$ne": ''}})
+for voter in voters:
+   contact = {"PHONE_NUM": voter["PHONE_NUM"]}
+   for key in interesting_keys:
+       if key in voter:
+           contact[key] = voter[key]
+   
+   phone_col.insert_one(contact)
