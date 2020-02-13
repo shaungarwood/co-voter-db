51b40cb35edea1fa1e9a588318c32db98bbd1fd6
Shaun Garwood <shaun.garwood@gmail.com>
2020-02-13 05:51:27 -0700

    ugh, forgot )

--- drop.py
+++ drop.py
@@ -6,6 +6,6 @@ myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 mydb = myclient["voters"]
 mycol = mydb["2019-voters"]
 
-print(mycol.count_documents({})
+print(mycol.count_documents({}))
 mycol.drop()
-print(mycol.count_documents({})
+print(mycol.count_documents({}))
