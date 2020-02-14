d2bedd01366fd59b4efb3583b23fd77e181628ab
Shaun Garwood <shaun.garwood@gmail.com>
2020-02-14 05:50:01 -0700

    flask api

--- app.py
+++ app.py
@@ -1,48 +1,20 @@
-import re
 import json
-import pymongo
 
 from flask import Flask
 from flask import request
 
 app = Flask(__name__)
 
-client = pymongo.MongoClient("mongodb://localhost:27017/")
-db = client['voters']
-
-collection = db['2019-voters']
-
-def search(query):
-    res = []
-    for x in collection.find(query):
-        res.append(x.copy())
-    [clean_res(x) for x in res]
-    return res
-
-def search_name(name):
-    first, last = name.split(" ")
-    first = first.upper()
-    last = last.upper()
-    return search({"FIRST_NAME": first, "LAST_NAME": last})
-
-def search_phone(num):
-    num = str(num)
-    num = re.sub(r'[^\d]', '', num)
-    return search({"PHONE_NUM": num})
-
-def clean_res(entry): # i should do this the right way, copy
-    for key in list(entry):
-        if key in boring_fields or entry[key] == None:
-            del entry[key]
+import query
 
 @app.route('/search')
-def get():
-    documents = collection.find()
-    response = []
-    for document in documents:
-        document['_id'] = str(document['_id'])
-        response.append(document)
-    return json.dumps(response)
+def search():
+    if request.args and 'q' in request.args:
+        search_string = request.args['q']
+        res = query.determine_query_type(search_string)
+        return json.dumps(res)
+    else:
+        return "No query data received", 200
 
 if __name__ == '__main__':
     app.run()
