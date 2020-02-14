f2a65a13c2f1c5ba51a2cf75f90ec33283dd6ff2
Shaun Garwood <shaun.garwood@gmail.com>
2020-02-14 05:49:51 -0700

    made query into a module

--- query.py
+++ query.py
@@ -1,17 +1,12 @@
-#!/usr/bin/env python
-
-import pprint
 import re
 
 import pymongo
-from IPython import embed
 
-myclient = pymongo.MongoClient("mongodb://localhost:27017/")
-mydb = myclient["voters"]
-mycol = mydb["2019-voters"]
-#mycol = mydb["phone-nums"]
+client = pymongo.MongoClient("mongodb://localhost:27017/")
+db = client['voters']
 
-pp = pprint.PrettyPrinter(indent=4)
+voters = db['2019-voters']
+phones = db["phone-nums"]
 
 boring_fields = [
     "ADDRESS_LIBRARY_ID",
@@ -34,9 +29,10 @@ boring_fields = [
     "_id"
 ]
 
-def search(query):
+def dbsearch(query, collection=voters):
+    print(collection)
     res = []
-    for x in mycol.find(query):
+    for x in collection.find(query):
         res.append(x.copy())
     [clean_res(x) for x in res]
     return res
@@ -45,22 +41,45 @@ def search_name(name):
     first, last = name.split(" ")
     first = first.upper()
     last = last.upper()
-    return search({"FIRST_NAME": first, "LAST_NAME": last})
+    return dbsearch({"FIRST_NAME": first, "LAST_NAME": last})
 
 def search_phone(num):
     num = str(num)
     num = re.sub(r'[^\d]', '', num)
-    return search({"PHONE_NUM": num})
+    return dbsearch({"PHONE_NUM": num}, phones)
 
 def clean_res(entry): # i should do this the right way, copy
     for key in list(entry):
         if key in boring_fields or entry[key] == None:
             del entry[key]
 
-def print_res(res):
-    for x in res:
-        voter_copy = x.copy()
-        clean_res(voter_copy)
-        pp.pprint(voter_copy)
+def determine_query_type(search_string):
+    search_string = search_string.replace('_', ' ')
+    if len(re.findall(r'\d', search_string)) >= 10:
+        # phone
+        return search_phone(search_string)
+    elif re.search(r'^\d.+\w', search_string):
+        # address
+        pass
+    elif ' ' in search_string:
+        # name
+        return search_name(search_string)
+    else:
+        # unsure
+        print("unsure what to do with: ", search_string)
+        return []
 
-embed()
+if __name__ == '__main__':
+    import pprint
+
+    from IPython import embed
+
+    pp = pprint.PrettyPrinter(indent=4)
+
+    def print_res(res):
+        for x in res:
+            voter_copy = x.copy()
+            clean_res(voter_copy)
+            pp.pprint(voter_copy)
+
+    embed()
