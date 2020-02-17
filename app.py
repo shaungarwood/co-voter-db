5e54e4c27e8ccb46564aad93c07712cd7abc83de
Shaun Garwood <shaun.garwood@gmail.com>
2020-02-17 06:10:08 -0700

    updated app.py to use class

--- app.py
+++ app.py
@@ -2,19 +2,34 @@ import json
 
 from flask import Flask
 from flask import request
+from os import environ
+
+import query
 
 app = Flask(__name__)
 
-import query
+if 'MONGODB_HOST' in environ:
+    mongodb_host = environ['MONGODB_HOST']
+else:
+    mongodb_host = "localhost"
+
+if 'MONGODB_PORT' in environ:
+    mongodb_port = environ['MONGODB_PORT']
+else:
+    mongodb_port = "27017"
+
+vr = query.VoterRecords(mongodb_host, mongodb_port)
+
 
 @app.route('/search')
 def search():
     if request.args and 'q' in request.args:
         search_string = request.args['q']
-        res = query.determine_query_type(search_string)
+        res = vr.determine_query_type(search_string)
         return json.dumps(res)
     else:
         return "No query data received", 200
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
